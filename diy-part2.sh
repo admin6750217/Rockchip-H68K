ed9219f2260dc1f06984c5678b152f1faadb8a05
admin6750217 <58500194+admin6750217@users.noreply.github.com>
2026-09-07 22:10:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,41 +1,77 @@
 #!/bin/bash
-#===============================================
-# Description: DIY script
-# File name: diy-script.sh
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#===============================================
 
-# 修改uhttpd配置文件，启用nginx
-# sed -i "/.*uhttpd.*/d" .config
-# sed -i '/.*\/etc\/init.d.*/d' package/network/services/uhttpd/Makefile
-# sed -i '/.*.\/files\/uhttpd.init.*/d' package/network/services/uhttpd/Makefile
-sed -i "s/:80/:81/g" package/network/services/uhttpd/files/uhttpd.config
-sed -i "s/:443/:4443/g" package/network/services/uhttpd/files/uhttpd.config
-cp -a $GITHUB_WORKSPACE/configfiles/etc/* package/base-files/files/etc/
-# ls package/base-files/files/etc/
+# 删除feeds中的插件
+rm -rf ./feeds/packages/net/{geoview,chinadns-ng,hysteria,mosdns,v2ray-geodata,lucky}
+rm -rf ./feeds/packages/net/{shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev}
+rm -rf ./feeds/packages/net/{sing-box,v2ray-geodata,v2ray-plugin,xray-core,smartdns}
+
+rm -rf ./feeds/luci/applications/{luci-app-passwall,luci-app-passwall2,luci-app-openclash,luci-app-homeproxy}
+rm -rf ./feeds/luci/applications/{luci-app-lucky,luci-app-smartdns,luci-app-timecontrol,luci-app-mosdns}
+rm -rf ./feeds/luci/applications/{luci-app-nikki,luci-app-momo,luci-app-daed}
+
+# 克隆依赖插件
+git clone --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git package/pwpage
+# git clone --depth 1 https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 
 
-# 追加自定义内核配置项
-echo "CONFIG_PSI=y
-CONFIG_KPROBES=y" >> target/linux/rockchip/armv8/config-6.6
+# 克隆的源码放在small文件夹
+mkdir package/small
+pushd package/small
 
+# luci-theme-aurora
+git clone -b master --depth 1 https://github.com/eamonxg/luci-theme-aurora.git
 
-# 集成CPU性能跑分脚本
-cp -f $GITHUB_WORKSPACE/configfiles/coremark/coremark-arm64 package/base-files/files/bin/coremark-arm64
-cp -f $GITHUB_WORKSPACE/configfiles/coremark/coremark-arm64.sh package/base-files/files/bin/coremark.sh
-chmod 755 package/base-files/files/bin/coremark-arm64
-chmod 755 package/base-files/files/bin/coremark.sh
+# luci-app-nft-timecontrol
+git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-timecontrol.git
 
+# adguardhome
+# git clone -b 2024.09.05 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
 
-# 复制dts设备树文件到指定目录下
-cp -a $GITHUB_WORKSPACE/configfiles/dts/rk3588/* target/linux/rockchip/dts/rk3588/
+# homeproxy
+git clone -b master --depth 1 https://github.com/immortalwrt/homeproxy.git
 
+# lucky
+git clone -b main --depth 1 https://github.com/gdy666/luci-app-lucky.git
 
-# iStoreOS-settings
-git clone --depth=1 -b main https://github.com/xiaomeng9597/istoreos-settings package/default-settings
+# smartdns
+git clone -b master --depth 1 https://github.com/pymumu/luci-app-smartdns.git
+git clone -b master --depth 1 https://github.com/pymumu/smartdns.git
+sed -i 's@include ../../lang/rust/rust-package.mk@include $(TOPDIR)/feeds/packages/lang/rust/rust-package.mk@g' smartdns/package/openwrt/Makefile
+sed -n '33p' smartdns/package/openwrt/Makefile
 
+# ssrp
+# git clone -b master --depth 1 https://github.com/fw876/helloworld.git
 
-# 定时限速插件
-git clone --depth=1 https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
+# VIKINGYFY/packages
+git clone -b main --depth 1 https://github.com/VIKINGYFY/packages.git
+
+# passwall
+git clone -b main --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall.git
+
+# passwall2
+git clone -b main --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall2.git
+
+# mosdns
+git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
+
+# luci-app-netspeedtest
+git clone -b master --depth 1 https://github.com/sirpdboy/luci-app-netspeedtest.git
+
+# openclash
+git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git
+
+# OpenWrt-nikki
+git clone -b main --depth 1 https://github.com/nikkinikki-org/OpenWrt-nikki.git
+
+# OpenWrt-momo
+git clone -b main --depth 1 https://github.com/nikkinikki-org/OpenWrt-momo.git
+
+# daed
+git clone -b master --depth 1 https://github.com/QiuSimons/luci-app-daed.git
+
+#modem
+# git clone -b main --depth 1 https://github.com/FUjr/modem_feeds.git
+
+popd
+
+echo "packages executed successfully!"
