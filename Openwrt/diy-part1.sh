2ed4e0f8aeda4049b998ea70b1e6a7bb05bc71da
googlewmb <58500194+googlewmb@users.noreply.github.com>
2026-09-12 11:47:51 +0800

    Update diy-part1.sh

--- Openwrt/diy-part1.sh
+++ Openwrt/diy-part1.sh
@@ -16,9 +16,9 @@ echo 'net.netfilter.nf_conntrack_max=655550' >> package/base-files/files/etc/sys
 # 添加 kenzok8 软件源
 # ============================================================
 
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '3i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '3i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 
 # ============================================================
