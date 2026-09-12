dfd35cce84a4b415feabbdce0b8293b6b3df7819
googlewmb <58500194+googlewmb@users.noreply.github.com>
2026-09-13 00:22:52 +0800

    Update diy-part1.sh

--- Openwrt/diy-part1.sh
+++ Openwrt/diy-part1.sh
@@ -14,38 +14,14 @@ echo 'net.netfilter.nf_conntrack_max=655550' >> package/base-files/files/etc/sys
 #sed -i '3i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 #sed -i '4i src-git op https://github.com/kiddin9/op-packages' feeds.conf.default
 
-# 删除官方冲突包
-# rm -rf feeds/luci/applications/luci-app-passwall
-# rm -rf feeds/luci/applications/luci-app-passwall2
-# rm -rf feeds/luci/applications/luci-app-openclash
-# rm -rf feeds/luci/applications/luci-app-homeproxy
-# rm -rf feeds/luci/applications/luci-app-lucky
-# rm -rf feeds/luci/applications/luci-app-smartdns
-# rm -rf feeds/luci/applications/luci-app-mosdns
+# 删除 feeds 中的官方冲突包
+rm -rf ./feeds/packages/net/{geoview,chinadns-ng,hysteria,mosdns,v2ray-geodata,lucky}
+rm -rf ./feeds/packages/net/{shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev}
+rm -rf ./feeds/packages/net/{sing-box,v2ray-geodata,v2ray-plugin,xray-core,smartdns}
 
-# rm -rf feeds/packages/net/xray-core
-# rm -rf feeds/packages/net/v2ray-geodata
-# rm -rf feeds/packages/net/sing-box
-# rm -rf feeds/packages/net/chinadns-ng
-# rm -rf feeds/packages/net/dns2socks
-# rm -rf feeds/packages/net/hysteria
-# rm -rf feeds/packages/net/ipt2socks
-# rm -rf feeds/packages/net/microsocks
-# rm -rf feeds/packages/net/naiveproxy
-# rm -rf feeds/packages/net/shadowsocks-rust
-# rm -rf feeds/packages/net/shadowsocksr-libev
-# rm -rf feeds/packages/net/simple-obfs
-# rm -rf feeds/packages/net/tcping
-# rm -rf feeds/packages/net/v2ray-plugin
-# rm -rf feeds/packages/net/xray-plugin
-# rm -rf feeds/packages/net/geoview
-# rm -rf feeds/packages/net/shadow-tls
-# rm -rf feeds/packages/net/alist
-# rm -rf feeds/packages/net/adguardhome
-# rm -rf feeds/packages/net/mosdns
-# rm -rf feeds/packages/net/smartdns
-
-# rm -rf feeds/packages/utils/v2dat
+rm -rf ./feeds/luci/applications/{luci-app-passwall,luci-app-passwall2,luci-app-openclash,luci-app-homeproxy}
+rm -rf ./feeds/luci/applications/{luci-app-lucky,luci-app-smartdns,luci-app-timecontrol,luci-app-mosdns}
+rm -rf ./feeds/luci/applications/{luci-app-nikki,luci-app-momo,luci-app-daed}
 
 # Golang
 rm -rf feeds/packages/lang/golang
@@ -60,6 +36,89 @@ rm -rf package/passwall-packages
 git clone --depth 1 \
 https://github.com/Openwrt-Passwall/openwrt-passwall-packages \
 package/passwall-packages
+
+# 第三方软件源
+mkdir -p package/small
+cd package/small
+
+# PassWall
+git clone -b main --depth 1 \
+https://github.com/Openwrt-Passwall/openwrt-passwall.git
+
+# PassWall2
+git clone -b main --depth 1 \
+https://github.com/Openwrt-Passwall/openwrt-passwall2.git
+
+# SmartDNS
+git clone -b master --depth 1 \
+https://github.com/pymumu/luci-app-smartdns.git
+
+git clone -b master --depth 1 \
+https://github.com/pymumu/smartdns.git
+
+sed -i \
+'s@include ../../lang/rust/rust-package.mk@include $(TOPDIR)/feeds/packages/lang/rust/rust-package.mk@g' \
+smartdns/package/openwrt/Makefile
+
+sed -n '33p' smartdns/package/openwrt/Makefile
+
+# MosDNS
+git clone -b v5 --depth 1 \
+https://github.com/sbwml/luci-app-mosdns.git
+
+# OpenClash
+git clone -b master --depth 1 \
+https://github.com/vernesong/OpenClash.git
+
+# HomeProxy
+git clone -b master --depth 1 \
+https://github.com/immortalwrt/homeproxy.git
+
+# Lucky
+git clone -b main --depth 1 \
+https://github.com/gdy666/luci-app-lucky.git
+
+# TimeControl
+git clone -b main --depth 1 \
+https://github.com/sirpdboy/luci-app-timecontrol.git
+
+# NetSpeedTest
+git clone -b master --depth 1 \
+https://github.com/sirpdboy/luci-app-netspeedtest.git
+
+# Nikki
+git clone -b main --depth 1 \
+https://github.com/nikkinikki-org/OpenWrt-nikki.git
+
+# Momo
+git clone -b main --depth 1 \
+https://github.com/nikkinikki-org/OpenWrt-momo.git
+
+# Daed
+git clone -b master --depth 1 \
+https://github.com/QiuSimons/luci-app-daed.git
+
+# Aurora Theme
+git clone -b master --depth 1 \
+https://github.com/eamonxg/luci-theme-aurora.git
+
+# VIKINGYFY Packages
+git clone -b main --depth 1 \
+https://github.com/VIKINGYFY/packages.git
+
+# AdGuardHome
+# git clone -b 2024.09.05 --depth 1 \
+# https://github.com/XiaoBinin/luci-app-adguardhome.git
+
+# SSRP
+# git clone -b master --depth 1 \
+# https://github.com/fw876/helloworld.git
+
+# Modem
+# git clone -b main --depth 1 \
+# https://github.com/FUjr/modem_feeds.git
+
+cd ../..
 
 # 更新 feeds
 ./scripts/feeds update -a
