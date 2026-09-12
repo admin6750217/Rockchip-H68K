2017c5f07df5878064219d0710db1c0f1019aacb
googlewmb <58500194+googlewmb@users.noreply.github.com>
2026-09-12 19:48:40 +0800

    Update diy-part1.sh

--- Openwrt/diy-part1.sh
+++ Openwrt/diy-part1.sh
@@ -9,10 +9,10 @@ sed -i '/^[[:space:]]*net\.netfilter\.nf_conntrack_max[[:space:]]*=/d' package/b
 echo 'net.netfilter.nf_conntrack_max=655550' >> package/base-files/files/etc/sysctl.conf
 
 # 添加软件源
-#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '3i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-sed -i '4i src-git op https://github.com/kiddin9/op-packages' feeds.conf.default
+#sed -i '4i src-git op https://github.com/kiddin9/op-packages' feeds.conf.default
 
 # 删除官方冲突包
 rm -rf feeds/luci/applications/luci-app-passwall
